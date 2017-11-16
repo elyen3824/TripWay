2e4cfbbd78912f2941504d0bebb38f1cdee2c9c0
elyen3824 <you@example.com>
2017-11-16 11:55:02 +0000

    refactor unit test

---
+++ TripWay.Test/GraphShould.cs
@@ -0,0 +1,63 @@
+using TripWay.Library;
+using Xunit;
+
+namespace TripWay.Test
+{
+    public class GraphShould
+    {
+        private Graph graph;
+        private Vertex effeilTowerVertex;
+        private Vertex trocaderoVertex;
+        private Vertex arcDeTriomphe;
+        private Vertex montmartre;
+        private WeightedEdge effeilTowerToTrocaderoEdge;
+        private WeightedEdge effeilTowerToArcDeTriomphe;
+        private WeightedEdge trocaderoToArcDeTriomphe;
+        private WeightedEdge arcDeTriompheToMontmartre;
+
+        public GraphShould()
+        {
+            graph = new Graph("Test Graph");
+            InitVertices(graph);
+            InitEdges(graph);
+        }
+
+        private void InitVertices(Graph graph)
+        {
+            effeilTowerVertex = new Vertex("Effeil Tower");
+            trocaderoVertex = new Vertex("Trocadero");
+            arcDeTriomphe = new Vertex("Arc de Triomphe");
+            montmartre = new Vertex("Montmartre");
+
+            graph.AddVertex(effeilTowerVertex);
+            graph.AddVertex(trocaderoVertex);
+            graph.AddVertex(arcDeTriomphe);
+            graph.AddVertex(montmartre);
+        }
+
+        private void InitEdges(Graph graph)
+        {
+            effeilTowerToTrocaderoEdge = new WeightedEdge(effeilTowerVertex, trocaderoVertex, 2);
+            effeilTowerToArcDeTriomphe = new WeightedEdge(effeilTowerVertex, arcDeTriomphe, 4);
+            trocaderoToArcDeTriomphe = new WeightedEdge(trocaderoVertex, arcDeTriomphe, 2);
+            arcDeTriompheToMontmartre = new WeightedEdge(arcDeTriomphe, montmartre, 8);
+
+            graph.AddEdge(effeilTowerToTrocaderoEdge);
+            graph.AddEdge(effeilTowerToArcDeTriomphe);
+            graph.AddEdge(trocaderoToArcDeTriomphe);
+            graph.AddEdge(arcDeTriompheToMontmartre);
+
+        }
+
+        [Fact]
+        public void Find_The_Minimum_Spanning_Tree_Path_To_Go_Throught_All_Edges_Using_Less_Weighted_Edges()
+        {
+            var result = graph.MinimumSpanningTree(effeilTowerVertex);
+
+            Assert.True(result.Count == 3);
+            Assert.True(result[0] == effeilTowerToTrocaderoEdge);
+            Assert.True(result[1] == trocaderoToArcDeTriomphe);
+            Assert.True(result[2] == arcDeTriompheToMontmartre);
+        }
+    }
+}
